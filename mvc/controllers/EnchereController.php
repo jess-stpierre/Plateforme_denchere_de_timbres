8a85865ee4aab15eec209b3f16d50ecb2b8b4b93
Jessica <jessica.stpierregagne@gmail.com>
2025-12-09 17:48:48 -0500

    fini et tester fiche denchere

--- mvc/controllers/EnchereController.php
+++ mvc/controllers/EnchereController.php
@@ -5,6 +5,9 @@ namespace App\Controllers;
 use App\Models\Timbre;
 use App\Models\Image;
 use App\Models\Enchere;
+use App\Models\Conditions;
+use App\Models\Couleur;
+use App\Models\PaysOrigine;
 
 use App\Providers\View;
 use App\Providers\Auth;
@@ -47,5 +50,60 @@ class EnchereController {
         }
 
         return View::render("enchere/index", ['datas' => $datas]);
+    }
+
+    public function show($data = []){
+
+        Auth::session();
+
+        if(isset($data['id']) && $data['id'] != null){
+
+            $enchere_id = $data['id'];
+            $enchere = new Enchere;
+            $enchereSelectId = $enchere->selectId($enchere_id);
+
+            $timbre_id = $enchereSelectId['timbre_id'];
+
+            $timbre = new Timbre;
+            $timbreSelectId = $timbre->selectId($timbre_id);
+
+            $date_de_debut = $enchereSelectId['date_de_debut'];
+            $date_de_fin = $enchereSelectId['date_de_fin'];
+            $debut = new DateTime($date_de_debut);
+            $fin = new DateTime($date_de_fin);
+            $difference = $debut->diff($fin);
+            $temps = $difference->format('%a days, %h hours, %i minutes');
+
+            if($timbreSelectId) {
+
+                $image = new Image;
+                $imageSelect = $image->selectWhere('timbre_id', $timbre_id, 'ordre_daffichage');
+
+                $couleur_id = $timbreSelectId['couleur_id'];
+                $couleur = new Couleur;
+                $selectCouleur = $couleur->selectId($couleur_id);
+                $couleurName = $selectCouleur['nom'];
+
+                $pays_id = $timbreSelectId['pays_dorigine_id'];
+                $pays = new PaysOrigine;
+                $selectPays = $pays->selectId($pays_id);
+                $paysdOrigine = $selectPays['nom'];
+
+                $cond_id = $timbreSelectId['conditions_id'];
+                $condition = new Conditions;
+                $selectCond = $condition->selectId($cond_id);
+                $conditionName = $selectCond['nom'];
+
+                return View::render('enchere/show', ['enchere' => $enchereSelectId, 'temps' => $temps, 'timbre' => $timbreSelectId, 'images' => $imageSelect, 'couleur' => $couleurName, 'pays' => $paysdOrigine, 'condition' => $conditionName]);
+            }
+            else {
+
+                return View::render('error', ['msg' => 'Enchere pas trouvee!']);
+            }
+        }
+        else {
+
+            return View::render('error', ['msg' => '404 page pas trouvee!']);
+        }
     }
 }
---
+++ mvc/views/enchere/show.php
@@ -0,0 +1,72 @@
+{{ include('layouts/header.php', {title:"Détail d'Enchère"})}}
+
+<main id="contenu-principal" class="flex-gap centre colonnes">
+    <div class="resultats flex-gap">
+        <h1>Détail d'Enchère</h1>
+    </div>
+    <div>
+        <section class="bordure">
+            <h2 class="flex-gap centre">{{ timbre.nom }}</h2>
+            <div class="flex-gap centre">
+                <div class="flex-gap colonnes">
+                {% for image in images %}
+                    <img src="{{ asset ~ image.image_url }}" class="photo-galerie" alt="{{ image.description_courte }}">
+                {% endfor %}
+                </div>
+                <div class="flex-gap colonnes colonnes-petit">
+                    <img id="image-primaire" src="{{ asset ~ images[0].image_url }}" alt="{{ images[0].description_courte }}">
+                    <div id="fleches-changement" class="flex-gap">
+                        <a href="#"><</a>
+                        <a href="#">(+)</a>
+                        <a href="#">></a>
+                    </div>
+                    <p class="flex-gap centre">{{ timbre.description }}</p>
+                </div>
+                <section class="flex-gap colonnes centre">
+                    {% if enchere.coups_de_coeur == 1 %}
+                        <h3>*COUPS DE COEUR DU LORD*</h3>
+                    {% endif %}
+                    <span>Temps restant: <span>{{ temps }}</span></span>
+                    <div class="flex-gap colonnes">
+                        <div id="offre" class="conteneur flex-gap colonnes centre">
+                            <p>Prix plancher: <span>{{ enchere.prix_plancher }}</span>$</p>
+                            <p>Offre actuelle: <span>{{ enchere.prix_courant }}</span>$</p>
+                            <p>par: <span>*X*</span></p>
+                            <div id="prix" class="bg-input">
+                                <label for="cad" class="visuellement-cache">Le montant de CAD$ que vous voulez misez</label>
+                                <input class="bg-input" type="number" name="cad" id="cad" placeholder="Faire une mise - $CAD">
+                            </div>
+                        </div>
+                        <div id="mise" class="conteneur flex-gap colonnes centre">
+                            <p>Nombres de mises: <span>*X*</span></p>
+                            <a href="#">Placer une mise</a>
+                        </div>
+
+                    </div>
+                </section>
+            </div>
+        </section>
+        <section class="bordure">
+            <h2 class="flex-gap centre">Details du Timbre</h2>
+            <div class="flex-gap centre detail-du-timbre">
+                <div>
+                    <p>Date de création: <span>{{ timbre.date_de_creation}}</span></p>
+                    <p>Pays d’origine: <span>{{ pays }}</span></p>
+                    <p>Tirage: <span>{{ timbre.tirage }}</span></p>
+                    {% if timbre.certifie == 0 %}
+                        <p>Certifié: <span> Non </span></p>
+                    {% else %}
+                        <p>Certifié: <span> Oui </span></p>
+                    {% endif %}
+                </div>
+                <div>
+                    <p>Couleurs: <span>{{ couleur }}</span></p>
+                    <p>Condition: <span>{{ condition }}</span></p>
+                    <p>Dimensions: <span>{{ timbre.dimensions }}</span></p>
+                </div>
+            </div>
+        </section>
+    </div>
+</main>
+
+{{ include('layouts/footer.php')}}
